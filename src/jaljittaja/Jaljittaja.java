2b13a776ff8e96c78a2d5158ebc04a9657ee490d
Jouni Männistö <jouni.mannisto@gmail.com>
2015-03-13 19:21:38 +0200

    TiRa2015 aloitus. Projektin luonti

---
+++ src/jaljittaja/Jaljittaja.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jaljittaja;
+
+/**
+ *
+ * @author jouni
+ */
+public class Jaljittaja {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
